d53e3d39e2f4a5d3ee97c3548c5d1466b15b1be0
ridoy <mdrezuanislamridoy@gmail.com>
2024-05-01 00:49:46 +0600

    upload

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+
+import './App.css'
+import './sss/app.scss'
+
+function App() {
+
+
+  return (
+    <>
+      <div className='bg-gray-500 text-white p-4 rounded-md shadow-md'>
+        <h1 className='text-blue-400 text-4xl'>hello world</h1>
+      </div>
+    </>
+  )
+}
+
+export default App
